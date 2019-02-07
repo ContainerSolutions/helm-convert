6add80ec701aa085b5b7d482fd9fa9fb3bd67830
Etienne Tremel <etienne.tremel@container-solutions.com>
2019-02-07 10:28:44 +0100

    Add missing install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+current_version=$(sed -n -e 's/version:[ "]*\([^"]*\).*/\1/p' $(dirname $0)/plugin.yaml)
+HELM_CONVERT_VERSION=${HELM_CONVERT_VERSION:-$current_version}
+
+dir=${HELM_PLUGIN_DIR:-"$(helm home)/plugins/helm-convert"}
+os=$(uname -s | tr '[:upper:]' '[:lower:]')
+release_file="helm-convert_${os}_${HELM_CONVERT_VERSION}.tar.gz"
+url="https://github.com/ContainerSolutions/helm-convert/releases/download/v${HELM_CONVERT_VERSION}/${release_file}"
+
+mkdir -p $dir
+
+if command -v wget
+then
+  wget -O ${dir}/${release_file} $url
+elif command -v curl; then
+  curl -L -o ${dir}/${release_file} $url
+fi
+
+tar xvf ${dir}/${release_file} -C $dir
+
+chmod +x ${dir}/helm-convert
+
+rm ${dir}/${release_file}
